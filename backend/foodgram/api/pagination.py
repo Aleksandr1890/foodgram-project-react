cff24495e1a2b1d6c9205065570fc27544de5bba
Александр Семенов <2918@list.ru>
2023-06-30 18:37:25 +0300

    fix_bugs

--- backend/foodgram/api/pagination.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class CustomPageSizePagination(PageNumberPagination):
-    """Пагинатор для параметра limit"""
-    page_size_query_param = 'limit'
