9745b3f80000f3c5f2d718722d57f8d69fa2524a
Александр Семенов <2918@list.ru>
2023-07-01 13:33:41 +0300

    fix_load_csv

--- backend/recipes/management/commands/load_ingrs.py
+++ backend/recipes/management/commands/load_ingrs.py
@@ -7,17 +7,6 @@ from recipes.models import Ingredient
 
 class Command(BaseCommand):
     help = 'Загрузка данных из csv файла'
-#
-#    def handle(self, *args, **kwargs):
-#        with open(
-#            './data/ingredients.csv',
-#            'r',
-#            encoding='utf-8'
-#        ) as file:
-#            reader = DictReader(file)
-#            Ingredient.objects.bulk_create(
-#                Ingredient(**data) for data in reader)
-#        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
 
     def handle(self, *args, **options):
         with open(
@@ -26,6 +15,9 @@ class Command(BaseCommand):
                 encoding='utf-8'
         ) as file:
             reader = DictReader(file)
-            Ingredient.objects.bulk_create(
-                Ingredient(**data) for data in reader)
-        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
+            next(reader)
+            for row in reader:
+                Ingredient.objects.create(
+                    name=row[0],
+                    measurement_unit=row[1]
+                )
