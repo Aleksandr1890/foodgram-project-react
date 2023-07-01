d047729f2cc002f214f03ba4d292280331ef1444
Александр Семенов <2918@list.ru>
2023-07-01 16:48:34 +0300

    fix_frontend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -115,20 +115,20 @@ class SubscribeSerializer(CustomUserSerializer):
         )
         read_only_fields = ('email', 'username', 'first_name', 'last_name')
 
-    def validate(self, data):
-        author = self.instance
-        user = self.context.get('request').user
-        if Follow.objects.filter(author=author, user=user).exists():
-            raise ValidationError(
-                detail='Вы уже подписаны на этого пользователя!',
-                code=status.HTTP_400_BAD_REQUEST
-            )
-        if user == author:
-            raise ValidationError(
-                detail='Вы не можете подписаться на самого себя!',
-                code=status.HTTP_400_BAD_REQUEST
-            )
-        return data
+    #def validate(self, data):
+    #    author = self.instance
+    #    user = self.context.get('request').user
+    #    if Follow.objects.filter(author=author, user=user).exists():
+    #        raise ValidationError(
+    #            detail='Вы уже подписаны на этого пользователя!',
+    #            code=status.HTTP_400_BAD_REQUEST
+    #        )
+    #    if user == author:
+    #        raise ValidationError(
+    #            detail='Вы не можете подписаться на самого себя!',
+    #            code=status.HTTP_400_BAD_REQUEST
+    #        )
+    #    return data
 
     def get_recipes_count(self, obj):
         return Recipe.objects.filter(author=obj).count()
