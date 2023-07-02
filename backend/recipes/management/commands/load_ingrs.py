5a448eb59a63b79b78778536a3d91cc162112813
Александр Семенов <2918@list.ru>
2023-07-02 16:19:48 +0300

    fix_all_bugs

--- backend/recipes/management/commands/load_ingrs.py
+++ backend/recipes/management/commands/load_ingrs.py
@@ -9,14 +9,14 @@ class Command(BaseCommand):
     help = 'Загрузка данных из csv файла'
 
     def handle(self, *args, **options):
-            with open(
-                './data/ingredients.csv',
-                'r',
-                encoding='utf-8'
-            ) as csv_file:
-                reader = csv.reader(csv_file, delimiter=',')
-                bulk_create_data = (
-                    Ingredient(name=row[0], measurement_unit=row[1])
-                    for row in reader
-                )
-                Ingredient.objects.bulk_create(bulk_create_data)
+        with open(
+            './data/ingredients.csv',
+            'r',
+            encoding='utf-8'
+        ) as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            bulk_create_data = (
+                Ingredient(name=row[0], measurement_unit=row[1])
+                for row in reader
+            )
+            Ingredient.objects.bulk_create(bulk_create_data)
